c610cd4813410451e3ea1497d9c45112c6580d65
888Andrey888 <andrey.reshetoff.88@gmail.com>
2023-04-15 12:28:02 +0600

    changed method of adding guroy

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,19 @@ public class Main {
     public static String[] attackType = {"Physical", "Magical", "Kinetic", "Medic", "Golem", "Lucky", "Berserk", "Thor"};
     public static Random random = new Random();
 
-    public static Hero[] heroes = new Hero[3];
+    public static Hero[] heroes = new Hero[4];//неизменная длинна массива связывает руки... тут конечно лучше использовать коллекции... но уж таковы условия))))
 
     public static void main(String[] args) {
         Boss boss = new Boss();
-        boss.setBossHealth(800);
-        boss.setBossDamage(50);
-        boss.setBossDefence(chooseAttackType());
-
+        createBoss(boss, 800, 50);
         System.out.println(boss.toString());
 
         //ДЗ на сообразительность
-        createHeroes(200, 300, 20, 50, 3);
+        int quantityHeroes = 3;
+        for (int i = 0; i < quantityHeroes; i++) {
+            createHeroes(i, 200, 300, 20, 40);
+        }
+        createHeroes(3,200, 300, 20, 50, "Berserk");
         for (int i = 0; i < heroes.length; i++) {
             System.out.println(heroes[i].toString());
         }
@@ -27,11 +28,25 @@ public class Main {
     }
 
     //ДЗ на сообразительность
-    public static void createHeroes(int minRandomValueForHealth, int maxRandomValueForHeals,
-                                    int minRandomValueForDamage, int maxRandomValueForDamage, int quantityHeroes) {
-        for (int i = 0; i < quantityHeroes; i++) {
-            heroes[i] = new Hero((random.nextInt(minRandomValueForHealth, maxRandomValueForHeals)),
-                    (random.nextInt(minRandomValueForDamage, maxRandomValueForDamage)), chooseAttackType());
-        }
+
+    //Метод для создания героя со случайным типом атаки
+    public static void createHeroes(int i, int minRandomValueForHealth, int maxRandomValueForHeals,
+                                    int minRandomValueForDamage, int maxRandomValueForDamage) {
+        heroes[i] = new Hero((random.nextInt(minRandomValueForHealth, maxRandomValueForHeals)),
+                (random.nextInt(minRandomValueForDamage, maxRandomValueForDamage)), chooseAttackType());
+    }
+
+    //Перегрузка метода создания героя с явным указанием типа атаки
+    public static void createHeroes(int i, int minRandomValueForHealth, int maxRandomValueForHeals,
+                                    int minRandomValueForDamage, int maxRandomValueForDamage, String attackType) {
+        heroes[i] = new Hero((random.nextInt(minRandomValueForHealth, maxRandomValueForHeals)),
+                (random.nextInt(minRandomValueForDamage, maxRandomValueForDamage)), attackType);
+    }
+
+    //Так же решил создать метод createBoss, вдруг нужно будет создавать еще боссов
+    public static void createBoss(Boss boss, int bossHeals, int bossDamage){
+        boss.setBossHealth(bossHeals);
+        boss.setBossDamage(bossDamage);
+        boss.setBossDefence(chooseAttackType());
     }
 }
